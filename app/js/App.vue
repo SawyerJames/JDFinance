8481fe7826ff7cc72fd7a710f1c5cb49941d4d8e
SawyerJames <sawyerjames0211@gmail.com>
2018-03-08 11:28:43 +0800

    上传代码测试一下

---
+++ app/js/App.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div class="title">
+    <p>这是一段代码</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="css" scoped>
+  .title{
+    background: red;
+  }
+</style>
